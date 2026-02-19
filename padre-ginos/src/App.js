61415a4501248bfbbe903b2db66fb2ac02a3aaa3
mlsagar <sagar.maharjan617571@outlook.com>
2026-02-19 18:37:00 -0500

    Add: pizza component to the project

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,8 +1,39 @@
-const App = () => {
+const Pizza = (props) => {
     return React.createElement(
         "div",
         {},
-        React.createElement("h1", {}, "Padre Gino's")
+        [
+            React.createElement("h1", {}, props.name),
+            React.createElement("p", {}, props.description)
+        ]
+    )
+}
+
+const App = () => {
+    return React.createElement("div",{}, [
+        React.createElement("h1", {}, "Padre Gino's"),
+        React.createElement(Pizza, {
+            name: "The Pepperoni Pizza",
+            description: "some dope pizza"
+        }),
+        React.createElement(Pizza, {
+            name: "Americano Pizza",
+            description: "some dope pizza"
+        }),
+        React.createElement(Pizza, {
+            name: "The Hawaiian Pizza",
+            description: "some dope pizza"
+        }),
+        React.createElement(Pizza, {
+            name: "Chicken Pizza",
+            description: "some dope pizza"
+        }),
+        React.createElement(Pizza, {
+            name: "The Pepperoni Pizza",
+            description: "some dope pizza"
+        }),
+
+    ]
     )
 }
 
