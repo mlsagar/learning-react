f38ad54fd0df960ffe597150ed2ebe01d9bf9640
mlsagar <sagar.maharjan617571@outlook.com>
2026-02-25 15:47:22 -0500

    Add: prettier in the project

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,41 +1,35 @@
 const Pizza = (props) => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, props.name),
-            React.createElement("p", {}, props.description)
-        ]
-    )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
 
 const App = () => {
-    return React.createElement("div",{}, [
-        React.createElement("h1", {}, "Padre Gino's"),
-        React.createElement(Pizza, {
-            name: "The Pepperoni Pizza",
-            description: "some dope pizza"
-        }),
-        React.createElement(Pizza, {
-            name: "Americano Pizza",
-            description: "some dope pizza"
-        }),
-        React.createElement(Pizza, {
-            name: "The Hawaiian Pizza",
-            description: "some dope pizza"
-        }),
-        React.createElement(Pizza, {
-            name: "Chicken Pizza",
-            description: "some dope pizza"
-        }),
-        React.createElement(Pizza, {
-            name: "The Pepperoni Pizza",
-            description: "some dope pizza"
-        }),
-
-    ]
-    )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "some dope pizza",
+    }),
+    React.createElement(Pizza, {
+      name: "Americano Pizza",
+      description: "some dope pizza",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian Pizza",
+      description: "some dope pizza",
+    }),
+    React.createElement(Pizza, {
+      name: "Chicken Pizza",
+      description: "some dope pizza",
+    }),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "some dope pizza",
+    }),
+  ]);
+};
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
