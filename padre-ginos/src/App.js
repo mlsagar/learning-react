cc1615f5b72b36c05f6307abad85f620b3a0f193
mlsagar <sagar.maharjan617571@outlook.com>
2026-02-25 18:34:49 -0500

    Add: pizza in separate jsx file

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import Pizza from "./Pizza";
 
-const Pizza = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("p", {}, props.description),
-  ]);
-};
 
 const App = () => {
   return React.createElement("div", {}, [
